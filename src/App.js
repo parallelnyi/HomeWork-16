6afef35d4060d5a1f0e77d095cf33341ab063914
Daniel <zadrot105217@gmail.com>
2022-08-23 15:41:36 +0600

    homework is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DUMMY_DATA = [
+	{ id: 'm1', title: 'Junior' },
+	{ id: 'm2', title: 'Middle' },
+	{ id: 'm3', title: 'Senior' },
+]
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			items: [],
+		}
+	}
+	getInputValue = ({ target: { value } }) => {
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				items:
+					DUMMY_DATA.some(
+						(item) =>
+							item.title.toLowerCase() === value.toLowerCase(),
+					) &&
+					!prevState.items.some(
+						(item) =>
+							item.title.toLowerCase() === value.toLowerCase(),
+					)
+						? prevState.items.concat({
+								title: value.toLowerCase(),
+								id: Math.random().toString(),
+						  })
+						: prevState.items,
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div className='container'>
+				{this.state.items.map((item) => {
+					return (
+						<div key={item.id} className='uppdatevalue'>
+							<h1>{item.title}</h1>
+						</div>
+					)
+				})}
+
+				<input
+					className='input'
+					type='text'
+					onChange={this.getInputValue}
+				/>
+				<div>
+					{DUMMY_DATA.map((item) => {
+						return (
+							<div className='static-items'>
+								<h1 key={item.id}>{item.title}</h1>
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
